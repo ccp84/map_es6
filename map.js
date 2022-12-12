7201d5fa585d771cb4acb5468c4c360275c193d8
ccp84 <ccp84@icloud.com>
2022-12-12 17:41:09 +0000

    finished lesson

--- map.js
+++ map.js
@@ -5,16 +5,31 @@
  */
 
 // Using a for loop
-
+let nums = [2, 4, 6, 8, 10]; 
+let nums2 = [];
+for(let i of nums) {
+  nums2.push(i*2);
+}
+console.log(nums2);
 
 // Using map()
 
+const multiplyByTwo = function (num) {
+  return num * 2;
+}
+
+const mapNums = nums.map(multiplyByTwo);
+console.log(mapNums);
 
 // Simplified w/ map()
 
+const simpNums = nums.map(function (num) {return num * 2})
+console.log(simpNums);
 
 // Simplfied w/ map() + arrow function
 
+const arwNums = nums.map(num => num *2)
+console.log(arwNums);
 
 // With objects:
 const students = [
@@ -37,3 +52,6 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+let mapStudents = students.map(student => [student.id, student.name]);
+console.log(mapStudents);
